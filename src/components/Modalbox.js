929df31a02f4e51594763e8bac19b3a92f385855
Bruno Fenzl <bruno.fenzl@tietoevry.com>
2023-01-13 23:33:46 +0100

    fix slider jumping bug and image sizes on modals

--- src/components/Modalbox.js
+++ src/components/Modalbox.js
@@ -7,41 +7,38 @@ const Modalbox = ({ close, value }) => {
       name: "Fan made Pokéball",
       tag: "3d modeling, rigging, animation",
       desc: [
-        { p: "It wouldn`t be a portfolio without some piece representing some of my childhood. Gotta Catch 'Em All!" },
-        { p: "This is a purely fan-made piece. I do not own any rights regarding Pokémon and the companys envolved" }
-      ]
+        {
+          p: "It wouldn`t be a portfolio without some piece representing some of my childhood. Gotta Catch 'Em All!",
+        },
+        {
+          p: "This is a purely fan-made piece. I do not own any rights regarding Pokémon and the companys envolved",
+        },
+      ],
     },
     {
       name: "Male Elf",
       tag: "cartoon, anatomy, study",
-      desc: [
-        { p: "Sculpture portrait study, made in Blender." }
-      ]
+      desc: [{ p: "Sculpture portrait study, made in Blender." }],
     },
     {
       name: "Female Elf 1",
-      tag: "cartoon, anatomy, study", 
-      desc: [
-        { p: ""}
-      ]
-
+      tag: "cartoon, anatomy, study",
+      desc: [{ p: "" }],
     },
     {
       name: "Female Elf 2",
       tag: "cartoon, anatomy, study",
-      desc: [
-        { p: "sculpture portrait study, made in Blender." }
-      ]
-
+      desc: [{ p: "sculpture portrait study, made in Blender." }],
     },
     {
       name: "Canary - Comission",
       tag: "3d modeling, rigging, animation",
       desc: [
-        { p: "Comission for artist Lynn Carone, to her video about the Samsara circle." }
-      ]
-
-    }/*,
+        {
+          p: "Comission for artist Lynn Carone, to her video about the Samsara circle.",
+        },
+      ],
+    } /*,
     {
       name: "",
       tag: "",
@@ -50,7 +47,7 @@ const Modalbox = ({ close, value }) => {
         { p: ""}
       ]
 
-    }*/
+    }*/,
   ];
 
   const [index, setIndex] = useState(value);
@@ -88,13 +85,9 @@ const Modalbox = ({ close, value }) => {
                     <p className="fn__cat">{d.tag}</p>
                     <h3 className="fn__title">{d.name}</h3>
                     <div className="img_holder">
-                      <img src="/img/thumb/square.jpg" alt="" />
-                      <div
-                        className="abs_img"
-                        data-bg-img={`/img/portfolio/${index}.jpg`}
-                      />
+                      <img src={`/img/portfolio/${index}.jpg`} alt="" />
                     </div>
-                    {d.desc.map((des,i) => (
+                    {d.desc.map((des, i) => (
                       <p key={i} className="fn__desc">
                         {des.p}
                       </p>
