23265954e8564f30289af0de74fc479061017339
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2025-09-14 20:53:33 -0300

    update react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+// Get the DOM container
+const container = document.getElementById('root');
+
+// Create a root
+const root = createRoot(container);
+
+// Render your app
+root.render(<App />);
